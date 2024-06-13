b6d6399b41a4a2273c4ad05f6437077aab246f11
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2024-06-13 14:58:03 +0300

    ИТОГОВОЕ ТЕСТИРОВАНИЕ

---
+++ Код Будущего 23-24/ИТОГОВЫЙ ТЕСТ/Подсчет количества.py
@@ -0,0 +1,6 @@
+print(len(
+    [x for x in range(1, int(input()) + 1) if x % 3 != 0 and x % 2 != sum(
+        [int(i) for i in str(x)]
+    ) % 2]
+))
+
---
+++ Код Будущего 23-24/ИТОГОВЫЙ ТЕСТ/Программа1.py
@@ -0,0 +1,14 @@
+s = input()
+n = int(input())
+for i in range(n):
+    pos = 0
+    while s.find('011', pos) != -1:
+        pos = s.find('011', pos)
+        s = s[: pos] + '210220' + s[pos + 3:]
+        pos += 5
+    pos = 0
+    while s.find('022', pos) != -1:
+        pos = s.find('022', pos)
+        s = s[: pos] + '201102' + s[pos + 3:]
+        pos += 6
+print(len(s))
